3d099274af5535db940db8aaf8345a0646609c8e
Joshua Jameson-Wallis <jjw@Joshuas-MacBook-Air.local>
2021-08-12 17:42:52 +0100

    Header nav almost complete

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// includes() method on hamrburger img src attr - if includes('close') then set to normal, etc 
+const header = document.querySelector('.header')
+const nav = document.querySelector('#mobile-nav')
+const hamburgerBtn = document.querySelector('#hamburger')
+const closeBtn = document.querySelector('#close-btn')
+const browserWidth = () => window.innerWidth
+ 
+header.addEventListener('click', e => {
+    if (browserWidth() < 1000) { // update later!
+        const target = e.target
+        if (target.matches('#hamburger') || target.matches('#close-btn') ) {
+            nav.classList.toggle('hidden')
+            // hamburgerBtn.classList.toggle('hidden')
+            // closeBtn.classList.toggle('hidden')
+        }
+    }
+})
