c291c0e56cab260dd19a09d575e8f2bde13b324f
Joshua Jameson-Wallis <jjw@Joshuas-MacBook-Air.local>
2021-08-19 22:15:57 +0100

    Refactored

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 const domSelect = el => document.querySelector(el)
 const domSelectAll = el => document.querySelectorAll(el)
-const header = domSelect('.header')
-const nav = domSelect('#mobile-nav')
-const hamburgerBtn = domSelect('#hamburger')
-const arrows = domSelectAll('.arrow')
+const [header, hamburgerBtn, arrows] = [domSelect('.header'), domSelect('#hamburger'), domSelectAll('.arrow')]
 const browserWidth = () => window.innerWidth
 const paths = { 
     hamburgerBtnPath: './images/icon-hamburger.svg',
@@ -14,34 +11,40 @@ const paths = {
  
 header.addEventListener('click', e => {
     const target = e.target
-    const navList = target.nextElementSibling
-    const arrow = target.firstElementChild
+    const [navList, arrow, nav] = [target.nextElementSibling, target.firstElementChild, domSelect('#mobile-nav')]
+    browserWidth() < 1000 && mobileBtn(target, nav) 
+    navShowHide(target, navList, arrow)
+})
 
-    if (browserWidth() < 1000) { 
-        if (target.matches('#hamburger') || target.matches('#close-btn')) {
-            const src = {
-                normal() { hamburgerBtn.src = paths.hamburgerBtnPath },
-                close() { hamburgerBtn.src = paths.closeBtnPath }
-            }
-            nav.classList.toggle('hamburger-hidden')
-            let btnType = hamburgerBtn.src.includes('close') ? 'normal' : 'close'
-            src[btnType]()
+function mobileBtn(target, nav) {
+    if (target.matches('#hamburger') || target.matches('#close-btn')) {
+        const src = {
+            normal() { hamburgerBtn.src = paths.hamburgerBtnPath },
+            close() { hamburgerBtn.src = paths.closeBtnPath }
         }
-    } 
+        src[hamburgerBtn.src.includes('close') ? 'normal' : 'close']()
+        nav.classList.toggle('hamburger-hidden')
+    }
+}
 
+function navShowHide(target, navList, arrow) {
     if (target.matches('.header-nav__btn')) {
         if (!navList.classList.contains('hamburger-hidden')) {
             navList.classList.add('hamburger-hidden')
             arrow.classList.remove('rotate')
         } else {
-            const navLists = domSelectAll('.header-nav__ul')
-            for (const list of navLists) list.classList.add('hamburger-hidden')
-            for (const arrow of arrows) arrow.classList.remove('rotate')
+            reset()
             navList.classList.toggle('hamburger-hidden')
             arrow.classList.toggle('rotate')
         }
     }
-})
+}
+
+function reset() {
+    const navLists = domSelectAll('.header-nav__ul')
+    for (const list of navLists) list.classList.add('hamburger-hidden')
+    for (const arrow of arrows) arrow.classList.remove('rotate')
+}
 
 function createWhiteArrows() {
     for (const arrow of arrows) arrow.src = browserWidth() >= 1000 ? paths.lightArrow : paths.darkArrow
